ff48d46d88bb4ab7ea06cf01f31fba7732496d92
Riley <rileyc.gamer@gmail.com>
2022-07-26 22:53:16 -0400

    First screen functional; add UI changes and copy this screen in order to make other screens.

--- GaiaVisionMVP/ViewController.swift
+++ GaiaVisionMVP/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: UIImageView!
+    @IBOutlet weak var optionsForMap: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.image = UIImage(named:"washingtontownship.png")
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func optionsForMapEvents(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex{
+        case 0:
+            mapView.image = UIImage(named: "Canadian Geographic.jpeg")
+        case 1:
+            mapView.image = UIImage(named: "The Narwhal.jpeg")
+        case 2:
+            mapView.image = UIImage(named: "governmentofCanada.jpeg")
+        default:
+            mapView.image = nil
+        }
+    
 }
-
+}
