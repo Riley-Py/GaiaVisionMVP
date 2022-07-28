a767787ad1ff82e2508cd3696036bbb4f08b7289
Riley <rileyc.gamer@gmail.com>
2022-07-27 20:39:56 -0400

    Multiple screens working; home stretch (design other screens, get some scrolling, voila!)

---
+++ GaiaVisionMVP/CausesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CausesViewController.swift
+//  GaiaVisionMVP
+//
+//  Created by RileyCant on 2022-07-27.
+//
+
+import UIKit
+
+class CausesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
